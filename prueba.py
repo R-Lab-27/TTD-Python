579ad824d2659a7bcecce87ad875b8073b870f6d
Ronald <ronaldrene91@gmail.com>
2025-11-07 23:07:43 -0300

    Solo se aclaró mejor el comentario

--- prueba.py
+++ prueba.py
@@ -8,5 +8,5 @@ options.binary_location = "/snap/firefox/current/usr/lib/firefox/firefox"
 service = Service(executable_path="/snap/bin/geckodriver")
 driver = webdriver.Firefox(service=service, options=options)
 
-#Esta es la única forma que ubuntu podemos ejecutar el firefox con selenium, por el problema de
+#Esta es la única forma en ubuntu que podemos ejecutar el firefox con selenium, por el problema de
 #que el gekodriver se encuentra en /snap/bin/gecodriver y el firefox en otro sitio.
