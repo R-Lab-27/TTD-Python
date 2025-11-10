564f16decfb13ea989d5f12740d686aba5cd4c25
Ronald <ronaldrene91@gmail.com>
2025-11-10 19:04:32 -0300

    Before a little refactor

--- functional_tests2.py
+++ functional_tests2.py
@@ -46,10 +46,15 @@ class NewVisitorTest(unittest.TestCase):
 
         #There is still a text box inviting her to add another item
         #She enters "Use peacock feathers to make a fly"
-        self.fail("Finish the Tests!")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         #The page updates again, and now shows both items on her list
-
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows],)
         #Satisfiedm she goes back to sleep
 
 if __name__ == '__main__':
