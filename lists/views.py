45bcc44ced9459b6e84f56f055c2c73dff10b634
Ronald <ronaldrene91@gmail.com>
2025-11-12 17:17:59 -0300

    BugSlapped some bugs

--- lists/views.py
+++ lists/views.py
@@ -1,20 +1,22 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
+
 from lists.models import Item, List
 
-# Create your views here.
+
 def home_page(request):
     return render(request, "home.html")
 
-def view_list(request, list_id):
-    our_list = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=our_list)
-    return render(request, "list.html", {"items": items, "list": our_list})
 
 def new_list(request):
     nulist = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=nulist)
     return redirect(f"/lists/{nulist.id}/")
+
+
+def view_list(request, list_id):
+    our_list = List.objects.get(id=list_id)
+    return render(request, "list.html", {"list": our_list})
+
 
 def add_item(request, list_id):
     our_list = List.objects.get(id=list_id)
