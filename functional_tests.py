ce886ac98813aa501ceb4b0065c41e6d733216a3
Ronald <ronaldrene91@gmail.com>
2025-11-13 11:45:48 -0300

    Añadido unos comentarios de explicación al archivo functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -2,12 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
+# Estas son las librerías necesarias para configurar correctamente el webdriver de selenium para trabajar con firefox
+# en Ubuntu. Necesitamos establecer explicitamente las rutas de firefox y el geckodriver, sino, no es posible hacer funcionar
+# Estos parametros se pueden establecer en el método setUp() del fucntional test, ya sea heredando
+# de LiveServerTestCase o StaticLiveServerTestCase etc.
+
 options = Options()
 options.binary_location = "/snap/firefox/current/usr/lib/firefox/firefox"
 
 service = Service(executable_path="/snap/bin/geckodriver")
 browser = webdriver.Firefox(service=service, options=options)
 browser.get("http://localhost:8000")
-
-assert "Congratulations!" in browser.title
-print("OK")
